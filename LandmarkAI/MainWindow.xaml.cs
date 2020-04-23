73bab8908af87c003da3c31ad32308267ca163fb
Chad Bonthuys <chad@atomixdev.com>
2020-04-23 13:01:26 +0100

    API calls, deserilising the object, Test Images

--- LandmarkAI/MainWindow.xaml.cs
+++ LandmarkAI/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using LandmarkAI.Classes;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,7 +32,46 @@ namespace LandmarkAI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*.jpeg;|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
 
+            // User has selected a file
+            if (dialog.ShowDialog() == true)
+            {
+                string fileName = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(fileName));
+
+                MakePredictionAsync(fileName);
+
+            }
+        }
+
+        private async void MakePredictionAsync(string fileName)
+        {
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v3.0/Prediction/97519b89-6d6c-45a1-9b45-9a93631f6972/classify/iterations/WPFMSCustomVisionModel1/image";
+            string predictionKey = "853a3beed0bf46bfb6febaebc44bb50d";
+            string contentType = "application/octet-stream";
+
+            var file = File.ReadAllBytes(fileName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", predictionKey);
+
+                using (var content = new ByteArrayContent(file))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(contentType);
+                   var response = await client.PostAsync(url, content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    List<Prediction> predictions = JsonConvert.DeserializeObject<CustomVision>(responseString).predictions.ToList();
+                    listviewPredictions.ItemsSource = predictions;
+                }
+            }
         }
     }
 }
